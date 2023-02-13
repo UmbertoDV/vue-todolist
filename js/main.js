ddcf8a3e13afb430af1119802b79875d85085696
UmbertoDV <biccoumby2002@hotmail.com>
2023-02-13 15:04:56 +0100

    rimuovi elemento

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			message: "Hello Vue!",
+			tasks: ["Butta la pasta", "Fai la tavola", "Magna el tutto"],
+			newTask: "",
 		};
+	},
+
+	methods: {
+		addTask() {},
+
+		removeTask(i) {
+			this.tasks.splice(i, 1);
+		},
 	},
 }).mount("#app");
